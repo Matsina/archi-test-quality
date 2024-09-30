dce920502d8af258c7b2c8191af705950cf076c3
Jérémie Fauveau <contact@jeremie-fauveau.fr>
2024-09-30 10:37:19 +0200

    Règles métier, récup body, interfaces

--- src/order/presentation/order.controller.ts
+++ src/order/presentation/order.controller.ts
@@ -1,4 +1,22 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  BadRequestException,
+} from '@nestjs/common';
+
+interface ItemDetail {
+  productName: string;
+  price: number;
+}
+
+interface CreateOrder {
+  items: ItemDetail[];
+  customerName: string;
+  shippingAddress: string;
+  invoiceAddress: string;
+}
 
 @Controller('/orders')
 export default class OrderController {
@@ -8,7 +26,32 @@ export default class OrderController {
   }
 
   @Post()
-  async create() {
-    return 'This action adds a new cat';
+  async createOrder(@Body() createOrderDto: CreateOrder): Promise<string> {
+    const { items, customerName, shippingAddress, invoiceAddress } =
+      createOrderDto;
+
+    if (
+      !customerName ||
+      !items ||
+      items.length === 0 ||
+      !shippingAddress ||
+      !invoiceAddress
+    ) {
+      throw new BadRequestException('Missing required fields');
+    }
+
+    if (items.length > 5) {
+      throw new BadRequestException(
+        'Cannot create order with more than 5 items',
+      );
+    }
+
+    const totalAmount = items.reduce((sum, item) => sum + item.price, 0);
+    if (totalAmount < 10) {
+      throw new BadRequestException(
+        'Cannot create order with total amount less than 10€',
+      );
+    }
+    return 'OK';
   }
 }
